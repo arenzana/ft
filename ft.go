5071251c27572d86342c7b2176f17c8753c2da39
Ismael Arenzana <ismael.arenzana@trnswrks.com>
2015-02-02 15:59:38 -0500

    Got a test for airport validation.

--- ft.go
+++ ft.go
@@ -4,6 +4,7 @@ import ( "os"
        "github.com/codegangsta/cli"
        "net/http"
        "io"
+       "fmt"
        "path/filepath"
 )
 
@@ -16,8 +17,12 @@ const author string = "Ismael Arenzana"
 const email string = "iarenzana@gmail.com"
 const appName string = "ft"
 const appDescription string = "Command-line flight tracker"
-const userHome string = GetEnv("HOME")
-const separator = os.PathSeparator
+var userHome string = os.Getenv("HOME")
+var baseDir string = filepath.Dir(userHome + "/.ft/")
+var outputFileAirports string = baseDir + "/airports.dat"
+var outputFileAirlines string = baseDir + "/airlines.dat"
+var outputFileRoutes string = baseDir + "/routes.dat"
+
 /*
 Main function
 */
@@ -35,7 +40,11 @@ func main(){
 		    ShortName: "a",
 		    Usage:     "Display Airport Information",
 		    Action: func(c *cli.Context) {
-		      println("Airport Information: ", c.Args().First())
+		      if _, err := os.Stat(outputFileAirports); os.IsNotExist(err) {
+				getStaticData()
+				}
+				validateAirportCode(c.Args()[0])
+
 		    },
   		},
   		{
@@ -43,25 +52,35 @@ func main(){
   			ShortName: "t",
   			Usage: "Track a Flight",
   			Action: func(c *cli.Context) {
-		      println("Flight Information: ", c.Args().First())
-		    },
+		      if _, err := os.Stat(outputFileRoutes); os.IsNotExist(err) {
+				getStaticData()
+				}
+			},
   		},
   		{
   			Name:	"airlineinfo",
   			ShortName: "l",
   			Usage: "Airline Information",
   			Action: func(c *cli.Context) {
-		      println("Airline Information: ", c.Args().First())
+		      if _, err := os.Stat(outputFileAirlines); os.IsNotExist(err) {
+				getStaticData()
+				}
+
 		    },
   		},
   	}
-//	app.Action = func (c *cli.Context) {
-
-	//Entry Point
-//		println ("Boomshakalaka")
-//	}
 
 app.Run(os.Args)
+}
+/*
+Download data from openflights.org to not make much use of the FlightAware API ($$)
+*/
+func getStaticData() int {
+	os.MkdirAll(baseDir, 0777)
+	downloadFromUrl("http://sourceforge.net/p/openflights/code/HEAD/tree/openflights/data/airports.dat",outputFileAirports)
+	downloadFromUrl("http://sourceforge.net/p/openflights/code/HEAD/tree/openflights/data/airlines.dat",outputFileAirlines)
+	downloadFromUrl("http://sourceforge.net/p/openflights/code/HEAD/tree/openflights/data/routes.dat",outputFileRoutes)
+	return 0
 }
 /*
 Validate airport code.
@@ -69,11 +88,43 @@ Validate airport code.
 1 - ICAO code.
 0 - IATA code.
 */
-func getStaticData() int {
-	var outputFileAirport string = userHome + "/."
-}
 func validateAirportCode (airportCode string) int {
-	var airportEval int = 2
 
+	var airportEval int = 2
+	var airportCodeBytes=len([]rune(airportCode))
+
+	if airportCodeBytes == 3 {
+		airportEval = 0
+	} else if airportCodeBytes == 4 {
+		airportEval = 1
+	} else {
+		airportEval = 2
+	}
 	return airportEval
+}
+
+func downloadFromUrl(url string,fileName string) {
+
+	// TODO: check file existence first with io.IsExist
+	output, err := os.Create(filepath.FromSlash(fileName))
+
+	if err != nil {
+		fmt.Println("Error while creating", fileName, "-", err)
+		return
+	}
+	defer output.Close()
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return
+	}
+	defer response.Body.Close()
+
+	bytesRead, err := io.Copy(output, response.Body)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return
+	}
+	_ = bytesRead
 }
--- ft_test.go
+++ ft_test.go
@@ -7,30 +7,41 @@ var airportTests = []struct {
 	airportICAOCode string
 	airportIATACode string
 
-	{
-		airportICAOCode="LEMD",
-		airportIATACode = "MAD"
+	}{
+	 {
+		airportICAOCode: "LEMD",
+		airportIATACode: "MAD",
 	},
 	{
-		airportICAOCode="KJFK"
-		airportIATACode = "JFK"
+		airportICAOCode: "KJFK",
+		airportIATACode: "JFK",
 	},
 	{
-		airportICAOCode="WSSS"
-		airportIATACode = "SIN"
+		airportICAOCode: "WSSS",
+		airportIATACode: "SIN",
 	},
 	{
-		airportICAOCode="YSSY"
-		airportIATACode = "SYD"
+		airportICAOCode: "YSSY",
+		airportIATACode: "SYD",
 	},
 	{
-		airportICAOCode="FACT"
-		airportIATACode = "CPT"
+		airportICAOCode: "FACT",
+		airportIATACode: "CPT",
 	},
 }
 
 func TestValidateAirportCode (t *testing.T) {
 	for i,test := range airportTests {
-		test.airportIATACode.Len = 4;
+		var actualICAO int = validateAirportCode(test.airportICAOCode)
+		var actualIATA int = validateAirportCode(test.airportIATACode)
+
+		if actualICAO != 1 {
+			t.Error("Expected 1, got ", actualICAO, "Test %d", i)
+		}
+		if actualIATA != 0 {
+			t.Error("Expected 0, got ", actualIATA, "Test %d", i)
+		}
+
 	}
 }
+
